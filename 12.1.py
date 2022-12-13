203acc5c790ec1df9800d8304f2cfda567aff83f
itsbudyn <jakub.kedziora@gmail.com>
2022-12-13 10:34:19 +0100

    12.1 now properly complete

--- 12.1.py
+++ 12.1.py
@@ -21,25 +21,25 @@ def path(s):
             height=ord(s[curr_y][curr_x])
 
             if curr_y+1 < len(s) and height + 1 >= ord(s[curr_y+1][curr_x]) and [curr_y+1,curr_x] not in paths[i]: #DOWN
-                if height==ord("z") and s[curr_y+1][curr_x]=="E": return paths[i]
+                if height>=ord("y") and s[curr_y+1][curr_x]=="E": return paths[i]
                 if [curr_y+1,curr_x] not in visited:
                     current_paths.append(paths[i]+[[curr_y+1,curr_x]])
                     visited.append([curr_y+1,curr_x])
 
             if curr_y > 0 and height + 1 >= ord(s[curr_y-1][curr_x]) and [curr_y-1,curr_x] not in paths[i]: #UP
-                if height==ord("z") and s[curr_y-1][curr_x]=="E": return paths[i]
+                if height>=ord("y") and s[curr_y-1][curr_x]=="E": return paths[i]
                 if [curr_y-1,curr_x] not in visited:
                     current_paths.append(paths[i]+[[curr_y-1,curr_x]])
                     visited.append([curr_y-1,curr_x])
 
             if curr_x+1 < len(s[0]) and height + 1 >= ord(s[curr_y][curr_x+1]) and [curr_y,curr_x+1] not in paths[i]: #RIGHT
-                if height==ord("z") and s[curr_y][curr_x+1]=="E": return paths[i]
+                if height>=ord("y") and s[curr_y][curr_x+1]=="E": return paths[i]
                 if [curr_y,curr_x+1] not in visited:
                     current_paths.append(paths[i]+[[curr_y,curr_x+1]])
                     visited.append([curr_y,curr_x+1])
 
             if curr_x > 0 and height + 1 >= ord(s[curr_y][curr_x-1]) and [curr_y,curr_x-1] not in paths[i]: #LEFT
-                if height==ord("z") and s[curr_y][curr_x-1]=="E": return paths[i]
+                if height>=ord("y") and s[curr_y][curr_x-1]=="E": return paths[i]
                 if [curr_y,curr_x-1] not in visited:                
                     current_paths.append(paths[i]+[[curr_y,curr_x-1]])
                     visited.append([curr_y,curr_x-1])
